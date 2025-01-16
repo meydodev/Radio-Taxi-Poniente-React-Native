697a9b82432f5eaca64e8564e65c26e6feddc656
meydodev <meydodev@gmail.com>
2025-01-16 19:00:51 +0100

    third commit

--- app/constants/config.js
+++ app/constants/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://192.168.68.103:3000';
-//export const API_URL = 'https://qfd3jkd0hanv.share.zrok.io';
+//export const API_URL = 'http://192.168.68.103:3000';
+export const API_URL = 'https://663lxfxzmeia.share.zrok.io';
